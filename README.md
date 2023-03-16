40adc887493af09c02566eac53b787425ac953b6
uqarni <108438834+uqarni@users.noreply.github.com>
2023-03-16 01:51:56 +0000

    need to figure out how to query the pinecone model

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # langchain-pinecone
 experiment with langchain and pinecone
+
+# how it works
+ingest a PDF
+langchain breaks it up into documents
+openai changes these into embeddings - literally a list of numbers. a giant vector in 1500-dimensional space
+pinecone stores these embeddings externally
+
+openai turns a question into an embedding; pinecone will return the embeddings most similar to that query
+openai will take those supplied embeddings and return an answer
+
+# to get detectron2 (doesnt work still)
+apt-get update && apt-get -y install pybind11-dev
+CC=clang CXX=clang++ ARCHFLAGS="-arch x86_64" python -m pip install 'git+https://github.com/facebookresearch/detectron2.git' --user
+???
+
+
+# pinecone
+remember, openai embeddings have 1536 dimensions
+
+# inspired by
+https://www.youtube.com/watch?v=h0DHDp1FbmQ&ab_channel=DataIndependent
--- main.py
+++ main.py
@@ -1,1 +1,48 @@
+#from langchain.document_loaders import OnlinePDFLoader
+#from langchain.text_splitter import RecursiveCharacterTextSplitter
+#from langchain.vectorstores import Pinecone
+#from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.llms import OpenAI 
+from langchain.chains.question_answering import load_qa_chain
+import pinecone
+import openai
+import os
 
+#initialize pinecone
+pinecone.init(
+    api_key = os.environ.get("PINECONE_API_KEY"),
+    environment= os.environ.get("PINECONE_API_ENV")
+)
+
+openai.api_key = os.environ.get("OPENAI_API_KEY")
+
+# loader = OnlinePDFLoader("https://wolfpaulus.com/wp-content/uploads/2017/05/field-guide-to-data-science.pdf")
+# data = loader.load()
+
+# print(f'You have {len(data)} docuemnt(s) in your data')
+# print(f'There are {len(data[0].page_content)} characters in your document')
+
+# text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
+# texts = text_splitter.split_documents(data)
+
+# print(f'Now you have {len(texts)} documents')
+# print(texts[4])
+
+# embeddings = OpenAIEmbeddings(openai_api_key = openai.api_key)
+
+index_name = 'langchain1'
+query = "What are examples of good data science teams?"
+
+#docsearch = Pinecone.from_texts([t.page_content for t in texts], embeddings, index_name = index_name)
+
+
+docs = docsearch.similarity_search(query, include_metadata = True)
+
+
+
+llm = OpenAI(temperature = 0, openai_api_key = os.environ.get("OPENAI_API_KEY"))
+chain = load_qa_chain(llm, chain_type = "stuff")
+
+query = "What is the advise stage of data maturity?"
+response = chain.run(input_documents=docs, question=query)
+print(response)
